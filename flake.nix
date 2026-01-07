be69a812fbe1e3d15011481e2b63e694b7cf06c2
Cathal Mullan <contact@cathal.dev>
2026-01-07 14:30:10 +0000

    Initial project setup

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "duramen";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  # nix flake show
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+
+    let
+      perSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+
+      systemPkgs = perSystem (
+        system:
+
+        import nixpkgs {
+          inherit system;
+
+          overlays = [
+            rust-overlay.overlays.default
+          ];
+        }
+      );
+
+      perSystemPkgs = f: perSystem (system: f (systemPkgs.${system}));
+    in
+    {
+      devShells = perSystemPkgs (pkgs: {
+        # nix develop
+        default = pkgs.mkShell {
+          name = "duramen-shell";
+
+          env = {
+            # Nix
+            NIX_PATH = "nixpkgs=${nixpkgs.outPath}";
+          };
+
+          buildInputs = with pkgs; [
+            # Rust
+            rust-bin.nightly.latest.rustfmt
+            (rust-bin.stable.latest.minimal.override {
+              extensions = [
+                "clippy"
+                "rust-analyzer"
+                "rust-src"
+              ];
+            })
+            taplo
+            vscode-extensions.vadimcn.vscode-lldb.adapter
+
+            # GitHub
+            zizmor
+
+            # Spellchecking
+            typos
+            typos-lsp
+
+            # Nix
+            nixfmt
+            nixd
+            nil
+          ];
+        };
+      });
+    };
+}
