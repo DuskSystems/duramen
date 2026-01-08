c30913c450d434e28811efc43b3b4070d2ee83fe
Cathal Mullan <contact@cathal.dev>
2026-01-08 16:18:13 +0000

    Plan out public API shape

--- src/policy.rs
+++ src/policy.rs
@@ -1,3 +1,7 @@
+#![expect(clippy::todo, clippy::missing_errors_doc, reason = "WIP")]
+
+use alloc::string::String;
+
 use syntree::{FlavorDefault, Node, Tree};
 
 mod lexer;
@@ -8,3 +12,121 @@ pub use syntax::PolicyKind;
 
 pub type PolicyTree = Tree<PolicyKind, FlavorDefault>;
 pub type PolicyNode<'a> = Node<'a, PolicyKind, FlavorDefault>;
+
+pub struct PolicyErrors;
+
+pub struct PolicySet;
+
+impl PolicySet {
+    pub fn parse(_source: &str) -> Result<Self, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "serde")]
+    pub fn from_serde_json(_json: &str) -> Result<Self, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "serde")]
+    pub fn to_serde_json(&self) -> Result<String, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "facet")]
+    pub fn from_facet_json(_json: &str) -> Result<Self, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "facet")]
+    pub fn to_facet_json(&self) -> Result<String, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "prost")]
+    pub fn from_prost_bytes<B: prost::bytes::Buf>(_bytes: B) -> Result<Self, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "prost")]
+    pub fn to_prost_bytes(&self) -> Result<prost::bytes::Bytes, PolicyErrors> {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+pub struct Policy;
+
+impl Policy {
+    pub fn parse(_source: &str) -> Result<Self, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "serde")]
+    pub fn from_serde_json(_json: &str) -> Result<Self, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "serde")]
+    pub fn to_serde_json(&self) -> Result<String, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "facet")]
+    pub fn from_facet_json(_json: &str) -> Result<Self, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "facet")]
+    pub fn to_facet_json(&self) -> Result<String, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "prost")]
+    pub fn from_prost_bytes<B: prost::bytes::Buf>(_bytes: B) -> Result<Self, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "prost")]
+    pub fn to_prost_bytes(&self) -> Result<prost::bytes::Bytes, PolicyErrors> {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+pub struct Template;
+
+impl Template {
+    pub fn parse(_source: &str) -> Result<Self, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "serde")]
+    pub fn from_serde_json(_json: &str) -> Result<Self, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "serde")]
+    pub fn to_serde_json(&self) -> Result<String, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "facet")]
+    pub fn from_facet_json(_json: &str) -> Result<Self, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "facet")]
+    pub fn to_facet_json(&self) -> Result<String, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "prost")]
+    pub fn from_prost_bytes<B: prost::bytes::Buf>(_bytes: B) -> Result<Self, PolicyErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "prost")]
+    pub fn to_prost_bytes(&self) -> Result<prost::bytes::Bytes, PolicyErrors> {
+        todo!()
+    }
+}
--- src/schema.rs
+++ src/schema.rs
@@ -1,3 +1,7 @@
+#![expect(clippy::todo, clippy::missing_errors_doc, reason = "WIP")]
+
+use alloc::string::String;
+
 use syntree::{FlavorDefault, Node, Tree};
 
 mod lexer;
@@ -8,3 +12,43 @@ pub use syntax::SchemaKind;
 
 pub type SchemaTree = Tree<SchemaKind, FlavorDefault>;
 pub type SchemaNode<'a> = Node<'a, SchemaKind, FlavorDefault>;
+
+pub struct SchemaErrors;
+
+pub struct Schema;
+
+impl Schema {
+    pub fn parse(_source: &str) -> Result<Self, SchemaErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "serde")]
+    pub fn from_serde_json(_json: &str) -> Result<Self, SchemaErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "serde")]
+    pub fn to_serde_json(&self) -> Result<String, SchemaErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "facet")]
+    pub fn from_facet_json(_json: &str) -> Result<Self, SchemaErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "facet")]
+    pub fn to_facet_json(&self) -> Result<String, SchemaErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "prost")]
+    pub fn from_prost_bytes<B: prost::bytes::Buf>(_bytes: B) -> Result<Self, SchemaErrors> {
+        todo!()
+    }
+
+    #[cfg(feature = "prost")]
+    pub fn to_prost_bytes(&self) -> Result<prost::bytes::Bytes, SchemaErrors> {
+        todo!()
+    }
+}
