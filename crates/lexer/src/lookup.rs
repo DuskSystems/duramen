1b875ad508ab7f9bf5548e822f5455301662cb16
Cathal Mullan <contact@cathal.dev>
2026-02-07 12:42:57 +0000

    perf: Use lookup tables for classification

---
+++ crates/lexer/src/lookup.rs
@@ -0,0 +1,46 @@
+#![expect(
+    clippy::indexing_slicing,
+    clippy::cast_possible_truncation,
+    clippy::manual_is_ascii_check,
+    reason = "Cannot fail"
+)]
+
+pub const IDENTIFIER_TABLE: [bool; 256] = {
+    let mut table = [false; 256];
+
+    let mut index = 0;
+    while index < 256 {
+        table[index] = matches!(
+            index as u8,
+            b'a'..=b'z' | b'A'..=b'Z' | b'0'..=b'9' | b'_'
+        );
+
+        index += 1;
+    }
+
+    table
+};
+
+pub const WHITESPACE_TABLE: [bool; 128] = {
+    let mut table = [false; 128];
+
+    let mut index = 0;
+    while index < 128 {
+        table[index] = matches!(index as u8, b' ' | b'\t' | b'\n' | b'\r' | 0x0B | 0x0C);
+        index += 1;
+    }
+
+    table
+};
+
+pub const INTEGER_TABLE: [bool; 256] = {
+    let mut table = [false; 256];
+
+    let mut index = 0;
+    while index < 256 {
+        table[index] = matches!(index as u8, b'0'..=b'9');
+        index += 1;
+    }
+
+    table
+};
